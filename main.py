e78f8e846c60fb47b8d95b4c1163c4eafaa70ad4
dedouwe26 <lucwesterik@outlook.com>
2023-12-04 11:57:31 +0100

    qq

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import discord
-from discord.ext import commands
+from discord import app_commands
 import os
 
 discord_token = ""
@@ -11,36 +11,18 @@ with open("discord.txt", "r") as discordfile:
 
 print("Invitation link: "+discord.utils.oauth_url(client_id, permissions=discord.Permissions(18479365422144)))
 
-intents = discord.Intents.all()
-intents.message_content = True
-bot = commands.Bot(command_prefix='/', description="It's a dungeon game in Discord.", intents=intents)
+client = discord.Client(intents=discord.Intents.all())
+tree = app_commands.CommandTree(client)
 
 # commandTree = discord.app_commands.CommandTree(bot, )
 
-@bot.event
+@client.event
 async def on_ready():
-    print(f'We have logged in as {bot.user}')
-    await bot.tree.sync()
+    print(f'We have logged in as {client.user}')
+    await tree.sync()
 
-# @bot.event
-# async def on_message(message):
-#     await bot.process_commands(message)
-#     if message.author == bot.user:
-#         return
-
-#     if message.content.startswith('$hello'):
-#         await message.channel.send('Hello!')
-async def argauto():
-    return "arg"
-@bot.hybrid_command()
-async def test(ctx, arg):
-    await ctx.send(arg)
-
-@bot.tree.command(name="opengame", description="Opens the game in that channel")
+@tree.command(name="opengame", description="Opens the game in that channel")
 async def opengame(ctx: discord.Interaction):
-    print("ok")
+    await ctx.channel.send("opened here")
 
-
-# async def opengameAutocomplete()
-
-bot.run(discord_token)
+client.run(discord_token)
