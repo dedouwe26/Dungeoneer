655f84ebd803f5fdcf43383f843ed0e34046c552
dedouwe26 <lucwesterik@outlook.com>
2023-12-09 14:19:06 +0100

    aa

--- Dungeoneer.py
+++ Dungeoneer.py
@@ -1,8 +1,11 @@
 import random
+from Player import *
+
+TileType = {"WALL": "■", "FLOOR": "", "BARRIER": "▨", "CHEST": "◊", "ENEMY": "⚉","ENEMY2": "⚇", "BASE": "⌂", "AID": "⛨"}
 
 class Seed:
     seed: list[int]
-    def __init__(self, seed = [random.randint(0,255) for _ in range(7)]):
+    def __init__(self, seed: list[int] = [random.randint(0,255) for _ in range(7)]):
         self.seed = seed
     def __str__(self) -> str:
         seed = "&"
@@ -13,18 +16,21 @@ class Seed:
         return random.Random(self.__str__())
 class Map:
     name: str
-    data: list[int]
-    hasChest: bool
+    data: list[list[str]]
     def __init__(self, name: str, seed: Seed):
         self.name = name
-        self.hasChest = bool(seed.GetRandom().getrandbits(1))
+    def __str__(self) -> str:
+        world=""
+        for row in self.data:
+            for char in row:
+                world+=char
+            world+="\n"
 
 class World:
-    def __init__(self, seed: Seed):
-        self.seed = seed
+    maps: list[Map]
+    def __init__(self):
+        pass
+
 
 if __name__ == "__main__":
-    worldseed = Seed()
-    mainmap = Map("mainmap", worldseed)
-    world = World(worldseed)
-    print(mainmap.hasChest)
+    pass
--- main.py
+++ main.py
@@ -10,7 +10,15 @@ print("Invitation link: "+discord.utils.oauth_url(client_id, permissions=discord
 client = discord.Client(intents=discord.Intents.all())
 tree = app_commands.CommandTree(client)
 
-# commandTree = discord.app_commands.CommandTree(bot, )
+class GameView(discord.ui.View):
+    @discord.ui.button(label="Click!!")
+    async def clickbutton(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await interaction.user.send("why didd you click??")
+
+class MyView(discord.ui.View): # Create a class called MyView that subclasses discord.ui.View
+    @discord.ui.button(label="Click me!", style=discord.ButtonStyle.primary, emoji="😎") # Create a button with the label "😎 Click me!" with color Blurple
+    async def button_callback(self, button, interaction):
+        await interaction.response.send_message("You clicked the button!")
 
 @client.event
 async def on_ready():
@@ -18,7 +26,9 @@ async def on_ready():
     await tree.sync()
 
 @tree.command(name="opengame", description="Opens the game in that channel")
-async def opengame(ctx: discord.Interaction):
-    await ctx.channel.send("opened here")
+async def opengame(interaction: discord.Interaction):
+    # gameEmbed = discord.embeds.Embed()
+    # gameEmbed.add_field(name="test", value="test")
+    a = await interaction.response.send_message("opened here", view=GameView())
 
 client.run(discord_token)
